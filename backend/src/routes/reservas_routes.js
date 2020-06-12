eed7efefef58ccc984cd85b86395c09f7fcfe439
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-06-11 21:55:06 -0300

    Update reservas_routes.js

--- backend/src/routes/reservas_routes.js
+++ backend/src/routes/reservas_routes.js
@@ -5,7 +5,24 @@ const router = express.Router();
 
 
 router.get('/', (req, res) =>{
-    
+    let sql = `SELECT *
+                FROM reservas`
+
+    cnn.query(sql,
+        function(err,result,fields){
+            
+            let respuesta;
+            if (err){
+                respuesta = {
+                                status:'error',
+                                message:'No se pudo enviar las reservas'
+                            }
+            }else{
+                res.json(result);
+            }
+
+        }
+    );
 })
 
 
