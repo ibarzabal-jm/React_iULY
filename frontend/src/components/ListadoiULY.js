90662d93f9c2316dae1f499ad2d55b0d44ed7650
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-06-20 00:39:52 -0300

    Update ListadoiULY.js

--- frontend/src/components/ListadoiULY.js
+++ frontend/src/components/ListadoiULY.js
@@ -1,61 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { MDBRow, MDBContainer} from 'mdbreact';
-import TarjetaiULY from './TarjetaiULY';
+import React, { useState, useEffect } from "react";
+import { MDBRow, MDBContainer } from "mdbreact";
+import TarjetaiULY from "./TarjetaiULY";
 
-const ListadoiULY = () =>{
+const ListadoiULY = () => {
+  const [mucamas, setMucamas] = useState([]);
 
-    const [ mucamas, setMucamas ] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8888/mucamas")
+      .then((response) => response.json())
+      .then((data) => {
+        setMucamas(data);
+      });
+  }, []);
 
-    useEffect ( ()=>{
-                    fetch('http://localhost:8888/mucamas').then(
-                            response => response.json()
-                    ).then(
-                        data => {
-                            setMucamas(data);
-                        }
-                    )
-                    
-                    ;
-                } , []
-    )
+  return (
+    <>
+      <MDBContainer fluid>
+        <MDBRow>
+          {
+            mucamas.map((mucama) => {
+                return (
+                    <TarjetaiULY
+                            id={mucama.mucama_id}
+                            nombre={mucama.nombre}
+                            imagen={mucama.foto}
+                            nacionalidad={mucama.nacionalidad}
+                            edad={mucama.nacimiento}
+                            alta={mucama.alta}
+                            modalidad="Con retiro"
 
-
-    return(
-        <>
-            <MDBContainer fluid>
-
-                <MDBRow>
-
-                    {
-                        mucamas.map( mucama => {
-
-                                            return(
-                                                
-                                                <TarjetaiULY    id={mucama.mucama_id}
-                                                                nombre={mucama.nombre}
-                                                                imagen={mucama.foto}
-                                                                nacionalidad={mucama.nacionalidad}
-                                                                edad={mucama.nacimiento}
-                                                                alta={mucama.alta}
-                                                                modalidad="Con retiro"
-
-                                
-                                                                limpieza="Limpieza"
-                                                                cocina="Cocina básica"
-                                                                cuidadonenes=""
-                                                                cuidadoancianos=""
-                                                                planchado="Planchado"
-                                
-                                                />
-                                            )
-                        })
-                    }
-                
-                </MDBRow>
-            </MDBContainer>
-           
-        </>
-    )
-}
+                            
+                            limpieza="Limpieza"
+                            cocina="Cocina básica"
+                            cuidadonenes=""
+                            cuidadoancianos=""
+                            planchado="Planchado"
+                    />
+                );
+            })
+          }
+        </MDBRow>
+      </MDBContainer>
+    </>
+  );
+};
 
 export default ListadoiULY;
+
